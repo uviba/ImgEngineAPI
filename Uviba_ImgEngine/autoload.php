3b4d433d55b27da761062d37153bd9774df26e4c
NewTimeBox <ibrahim.hasanov.98@gmail.com>
2017-10-13 11:44:41 +0400

    Update autoload.php

--- Uviba_ImgEngine/autoload.php
+++ Uviba_ImgEngine/autoload.php
@@ -1,7 +1,7 @@
 <?php 
 define("Uviba_Data_Url", 'https://api.uviba.com/data/api_key_data.php');
 define("Uviba_Images_Url", 'https://api.uviba.com/imge/');
-define("Uviba_UPLOAD_Url", 'https://api.uviba.com/Controller/upload.php');
+define("Uviba_UPLOAD_Url", 'https://api.uviba.com/imge/upload.php');
 define("KEY_FILE_PATH", __DIR__.'/storage/key.txt');
 require __DIR__.'/classes/Curl_process.php';
 require __DIR__.'/classes/ImgEngine.php';
